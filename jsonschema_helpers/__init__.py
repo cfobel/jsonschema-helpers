c208ec3f78fc5200fffceba427e7b3a529827052
Christian Fobel <christian@fobel.net>
2016-02-16 14:28:33 -0500

    [NB] Deprecate autoschema, superceeded by simpleschema

--- jsonschema_helpers/__init__.py
+++ jsonschema_helpers/__init__.py
@@ -66,70 +66,6 @@ class SimpleSchemaFunction(SchemaMixin, DecoratorMixin):
     #
     # [1]: http://stackoverflow.com/questions/22545339/callable-object-decorator-applied-to-method-doesnt-get-self-argument-on-input
     def __init__(self, function, simple_schema_override=None):
-        self.function = function
-        self.argspec = inspect.getargspec(function)
-
-        self.args = self.argspec.args or []
-
-        # Detect if function is a method, using technique described [here][1].
-        #
-        # [1]: http://stackoverflow.com/questions/8793233/python-can-a-decorator-determine-if-a-function-is-being-defined-inside-a-class
-        frames = inspect.stack()
-
-        class_name = None
-        for frame in frames[1:]:
-            if frame[3] == "<module>":
-                # At module level, go no further
-                break
-            elif '__module__' in frame[0].f_code.co_names:
-                class_name = frame[0].f_code.co_name
-                break
-        self.ismethod = (class_name is not None)
-
-        if self.ismethod:
-            self.unnamed_args = self.args[:1]
-            self.args = self.args[1:]
-        else:
-            self.unnamed_args = []
-        self.defaults = self.argspec.defaults or []
-        self.args_count = len(self.args)
-        self.default_count = len(self.defaults)
-        self.required_count = self.args_count - self.default_count
-
-        # Fill in "empty" schema definitions for args or kwargs not
-        # present in user-defined schema (if any).
-        simple_schema = dict([(k, {})
-                              for i, k in enumerate(self.args)])
-
-        if simple_schema_override:
-            simple_schema.update(simple_schema_override)
-
-        for i, k in enumerate(self.args[self.required_count:]):
-            if 'default' not in simple_schema[k]:
-                simple_schema[k]['default'] = self.defaults[i]
-
-        self.schema = {'type': 'object',
-                       'properties': simple_schema,
-                       'additionalProperties':
-                       (self.argspec.keywords is not None)}
-        if self.args:
-            self.schema['required'] = self.args
-        functools.update_wrapper(self, function)
-
-
-def simpleschema(simple_schema_override=None):
-    def simpleschema_closure(f):
-        return SimpleSchemaFunction(f, simple_schema_override)
-    return simpleschema_closure
-
-
-class AutoSchemaFunction(SchemaMixin, DecoratorMixin):
-    # Need to inherit from `DecoratorMixin` to support decorating methods.
-    # If `DecoratorMixin` is not used, decorated methods will not be passed
-    # instance as first argument.  See [here][1] for more information.
-    #
-    # [1]: http://stackoverflow.com/questions/22545339/callable-object-decorator-applied-to-method-doesnt-get-self-argument-on-input
-    def __init__(self, function, simple_schema_override=None):
         '''
         Interpret dictionary keyword argument default values as
         jsonschema schema property.
@@ -164,30 +100,33 @@ class AutoSchemaFunction(SchemaMixin, DecoratorMixin):
         self.default_count = len(self.defaults)
         self.required_count = self.args_count - self.default_count
 
-        # Fill in "empty" schema definitions for args or kwargs not
-        # present in user-defined schema (if any).
-        simple_schema = dict([(k, {})
-                              for i, k in enumerate(self.args)])
+        self.schema = self.create_schema(self.args, self.defaults,
+                                         self.required_count,
+                                         (self.argspec.keywords is not None))
 
-        for i, k in enumerate(self.args[self.required_count:]):
-            if isinstance(self.defaults[i], dict):
-                simple_schema[k] = self.defaults[i]
-            else:
-                simple_schema[k]['default'] = self.defaults[i]
-
-        if simple_schema_override:
-            simple_schema.update(simple_schema_override)
-
-        self.schema = {'type': 'object',
-                       'properties': simple_schema,
-                       'additionalProperties':
-                       (self.argspec.keywords is not None)}
         if self.args:
             self.schema['required'] = self.args
         functools.update_wrapper(self, function)
 
+    def create_schema(self, args, defaults, required_count, additional_kwargs, override=None):
+        # Fill in "empty" schema definitions for args or kwargs not
+        # present in user-defined schema (if any).
+        simple_schema = dict([(k, {}) for i, k in enumerate(args)])
 
-def autoschema(simple_schema_override=None):
+        if override:
+            simple_schema.update(override)
+
+        for i, k in enumerate(args[required_count:]):
+            if isinstance(defaults[i], dict):
+                simple_schema[k] = defaults[i]
+            elif 'default' not in simple_schema[k]:
+                simple_schema[k]['default'] = defaults[i]
+
+        return {'type': 'object', 'properties': simple_schema,
+                'additionalProperties': additional_kwargs}
+
+
+def simpleschema(simple_schema_override=None):
     def simpleschema_closure(f):
-        return AutoSchemaFunction(f, simple_schema_override)
+        return SimpleSchemaFunction(f, simple_schema_override)
     return simpleschema_closure
